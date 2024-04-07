3156eb60b2b2873f5c4b730618af53612792377e
PrasetioPangestu <prasetiopangestu11@gmail.com>
2024-04-07 19:53:48 +0700

    test dulu bang

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add Event on elements
@@ -14,9 +12,7 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
  * navbar toggle
@@ -30,18 +26,16 @@ const overlay = document.querySelector("[data-overlay]");
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
-}
+};
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
-
-
 
 /**
  * header & back top btn show when scroll down to 100px
@@ -58,6 +52,41 @@ const headerActive = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
+};
+
+// script.js
+function submitForm(event) {
+  event.preventDefault(); // Mencegah form untuk melakukan submit
+
+  // Mengambil nilai-nilai input dari form
+  var email = document.getElementById("email").value;
+  var nama = document.getElementById("nama").value;
+  var pesan = document.getElementById("pesan").value;
+
+  // Validasi form sebelum mengirim data
+  if (!validateEmail(email)) {
+    alert("Email tidak valid");
+    return;
+  }
+
+  // Kirim data ke alamat email menggunakan metode yang sesuai, misalnya Ajax atau API HTTP
+
+  // Contoh penggunaan JavaScript untuk mengirim email dengan menggunakan mailto:
+  var subject = "Form Kontak - " + nama;
+  var body =
+    "Email: " + email + "\n" + "Nama: " + nama + "\n" + "Pesan: " + pesan;
+  var mailtoLink =
+    "mailto:prasetiopangestu11@gmail.com?subject=" +
+    encodeURIComponent(subject) +
+    "&body=" +
+    encodeURIComponent(body);
+  window.location.href = mailtoLink;
+}
+
+// Validasi email menggunakan regular expression
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
 
 addEventOnElem(window, "scroll", headerActive);
